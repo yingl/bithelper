229f81475dfd0b78458f989d9f6616ea9e0397d5
yingl <linying_43151@163.com>
2018-01-12 15:43:07 +0800

    Create database.py

---
+++ okex_data/database.py
@@ -0,0 +1,44 @@
+import datetime as dt
+import traceback
+from peewee import BooleanField # peewee相关模块
+from peewee import CharField
+from peewee import DateTimeField
+from peewee import FloatField
+from peewee import Model
+from peewee import MySQLDatabase
+from peewee import TextField
+
+db = MySQLDatabase(host='localhost',
+                   user='root',
+                   passwd='',
+                   database='bithelper',
+                   charset='utf8')
+
+class btc_tops(Model):
+    data = TextField() # In json format
+    updated_at = DateTimeField(default=dt.datetime.now)
+    class Meta:
+        database = db
+
+class btc_bottoms(Model):
+    data = TextField()
+    updated_at = DateTimeField(default=dt.datetime.now)
+    class Meta:
+        database = db
+
+class btc_values(Model):
+    code = CharField()
+    price = FloatField()
+    updated_at = DateTimeField(default=dt.datetime.now)
+    class Meta:
+        database = db
+
+if __name__ == '__main__':
+    try:
+        db.connect()
+        db.create_tables([btc_tops, btc_bottoms, btc_values], safe=True)
+    except Exception as e:
+        print('%s\n%s' % (e, traceback.print_exc()))
+    finally:
+        if db:
+            db.close()
