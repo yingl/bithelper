09376fde8f6e85c129d7c01188a941f76efd6877
yingl <linying_43151@163.com>
2018-01-12 17:40:13 +0800

    Update util.class.php

--- wechat/lib/util.class.php
+++ wechat/lib/util.class.php
@@ -10,9 +10,19 @@ class Util {
     public function fetch_code($code) {
         $query = 'select * from btc_values where code = ? order by updated_at desc limit 1';
         $info = $this->dao->execute_with_fetch($query, array($code));
-        $price = $info[0]['price'];
-        $updated_at = $info[0]['updated_at'];
-        return ['price' => $price, 'updated_at' => $updated_at];
+        return $info[0];
+    }
+    
+    public function fetch_tops() {
+        $query = 'select * from btc_tops order by updated_at desc linit 1';
+        $info = $this->dao->execute_with_fetch($query);
+        return $info[0];
+    }
+    
+    public function fetch_bottoms() {
+        $query = 'select * from btc_bottoms order by updated_at desc linit 1';
+        $info = $this->dao->execute_with_fetch($query);
+        return $info[0];
     }
 }
 
