da92e270794a427eddd657db428fc54a05f3edad
yingl <linying_43151@163.com>
2018-01-15 13:18:06 +0800

    Update fetch_ticker.py

--- okex_data/fetch_ticker.py
+++ okex_data/fetch_ticker.py
@@ -1,7 +1,9 @@
 import argparse
+import logging
 import os
 import sys
 import time
+import traceback
 sys.path.append('./')
 import coins
 import util
@@ -21,26 +23,32 @@ def write_to_aggs(prices, tag):
     util.write_aggs('\n'.join(data), tag)
 
 async def fetch_ticker():
+    logging.basicConfig(filename=fetch_ticker.log_file, level=logging.INFO)
     session = fetch_ticker.session
     while True:
-        responses = []
-        for coin in coins.coins:
-            responses.append(await session.get('https://www.okex.com/api/v1/ticker.do?symbol=%s_btc' % coin))
-        prices = []
-        for response in responses:
-            jdata = response.json()
-            url = response.url
-            code = url.split('?')[1].split('=')[1].split('_')[0]
-            price = float(jdata['ticker']['last'])
-            prices.append({'code': code, 'price': price})
-            util.write_values(code, price)
-        prices = sorted(prices, key=lambda x: x['price'])
-        write_to_aggs(prices[-6:-1][::-1], 'bts_price_top5')
-        write_to_aggs(prices[:5], 'bts_price_bottom5')
+        try:
+            responses = []
+            for coin in coins.coins:
+                responses.append(await session.get('https://www.okex.com/api/v1/ticker.do?symbol=%s_btc' % coin))
+            prices = []
+            for response in responses:
+                jdata = response.json()
+                url = response.url
+                code = url.split('?')[1].split('=')[1].split('_')[0]
+                price = float(jdata['ticker']['last'])
+                prices.append({'code': code, 'price': price})
+                util.write_values(code, price)
+            prices = sorted(prices, key=lambda x: x['price'])
+            write_to_aggs(prices[-6:-1][::-1], 'bts_price_top5')
+            write_to_aggs(prices[:5], 'bts_price_bottom5')
+        except Exception as e:
+            logging.error(e)
+            logging.error(traceback.format_exc())
         time.sleep(45)
 
 if __name__ == '__main__':
     args = parse_args()
     util.daemonize(fetch_ticker,
                    os.path.join(os.getcwd(), 'pid_ticker.txt'),
+                   os.path.join(os.getcwd(), 'log_ticker.txt'),
                    args.debug)
