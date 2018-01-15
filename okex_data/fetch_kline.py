af57f6aebe272a8944965461b7eee4648100dc41
yingl <linying_43151@163.com>
2018-01-15 13:16:00 +0800

    Update fetch_kline.py

--- okex_data/fetch_kline.py
+++ okex_data/fetch_kline.py
@@ -1,7 +1,9 @@
 import argparse
+import logging
 import os
 import sys
 import time
+import traceback
 sys.path.append('./')
 import coins
 import util
@@ -22,30 +24,35 @@ def write_to_aggs(prices, tag):
     util.write_aggs('\n'.join(data), tag)
 
 async def fetch_kline():
+    logging.basicConfig(filename=fetch_kline.log_file, level=logging.INFO)
     session = fetch_kline.session
     ks = ['15min', '1hour']
     while True:
-        for k in ks:
-            responses = []
-            for coin in coins.coins:
-                url = 'https://www.okex.com/api/v1/kline.do?symbol=%s_btc&type=%s&size=1' % \
-                      (coin, k)
-                responses.append(await session.get(url)) 
-            prices = []
-            for response in responses:
-                jdata = response.json()[0] # Becasue it returns an array of arrays
-                url = response.url
-                code = url.split('?')[1].split('&')[0].split('=')[1].split('_')[0]
-                open_ = float(jdata[1])
-                close_ = float(jdata[4])
-                pct_chg = (close_ - open_) / open_ * 100.0 # all day trading...
-                prices.append({'code': code,
-                               'open': open_,
-                               'close': close_,
-                               'pct_chg': pct_chg})
-            prices = sorted(prices, key=lambda x: x['pct_chg'])
-            write_to_aggs(prices[-6:-1][::-1], 'bts_change_%s_top5' % k)
-            write_to_aggs(prices[:5], 'bts_change_%s_bottom5' % k)
+        try:
+            for k in ks:
+                responses = []
+                for coin in coins.coins:
+                    url = 'https://www.okex.com/api/v1/kline.do?symbol=%s_btc&type=%s&size=1' % \
+                          (coin, k)
+                    responses.append(await session.get(url)) 
+                prices = []
+                for response in responses:
+                    jdata = response.json()[0] # Becasue it returns an array of arrays
+                    url = response.url
+                    code = url.split('?')[1].split('&')[0].split('=')[1].split('_')[0]
+                    open_ = float(jdata[1])
+                    close_ = float(jdata[4])
+                    pct_chg = (close_ - open_) / open_ * 100.0 # all day trading...
+                    prices.append({'code': code,
+                                   'open': open_,
+                                   'close': close_,
+                                   'pct_chg': pct_chg})
+                prices = sorted(prices, key=lambda x: x['pct_chg'])
+                write_to_aggs(prices[-6:-1][::-1], 'bts_change_%s_top5' % k)
+                write_to_aggs(prices[:5], 'bts_change_%s_bottom5' % k)
+        except Exception as e:
+            logging.error(e)
+            logging.error(traceback.format_exc())
         time.sleep(60)
 
 if __name__ == '__main__':
